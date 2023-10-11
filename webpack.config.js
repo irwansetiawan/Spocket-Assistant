bee03d015c0ba3da25e7be218f7116d5146e5e94
Irwan Setiawan <irwan@irwansetiawan.com>
2023-10-11 20:44:15 +0800

    add postcss-preset-env

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@ const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
    mode: "production",
    entry: {
-      serviceworker: path.resolve(__dirname, "src", "background/serviceworker.ts"),
-      contentscript: path.resolve(__dirname, "src", "contentscripts/contentscript.ts"),
-      popup: path.resolve(__dirname, "src", "popup/popup.ts"),
+      serviceworker: path.resolve(__dirname, "src", "background", "serviceworker.ts"),
+      contentscript: path.resolve(__dirname, "src", "contentscripts", "contentscript.ts"),
+      popup: path.resolve(__dirname, "src", "popup", "popup.ts"),
    },
    output: {
       path: path.join(__dirname, "dist"),
